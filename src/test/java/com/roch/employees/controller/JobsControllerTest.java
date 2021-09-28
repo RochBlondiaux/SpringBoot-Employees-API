0cd6442ceaa8d35e71d2efb61af59478be682e04
Roch Blondiaux <contact@roch-blondiaux.com>
2021-09-28 23:12:48 +0200

    Tests
    Took 7 minutes

---
+++ src/test/java/com/roch/employees/controller/JobsControllerTest.java
@@ -0,0 +1,30 @@
+package com.roch.employees.controller;
+
+import com.roch.employees.service.JobService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+/**
+ * @author Roch Blondiaux
+ * www.roch-blondiaux.com
+ */
+@WebMvcTest(controllers = JobController.class)
+public class JobsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private JobService service;
+
+    @Test
+    public void testGetEmployees() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/jobs"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
