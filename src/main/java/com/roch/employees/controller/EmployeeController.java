9b85617d37e1e4162556b9e886aa6278c0f1dc63
Roch Blondiaux <contact@roch-blondiaux.com>
2021-09-29 15:20:51 +0200

    Employees Controller update & tests
    Took 35 minutes

--- src/main/java/com/roch/employees/controller/EmployeeController.java
+++ src/main/java/com/roch/employees/controller/EmployeeController.java
@@ -4,10 +4,14 @@ import com.roch.employees.exceptions.EmployeeNotFoundException;
 import com.roch.employees.model.Employee;
 import com.roch.employees.service.EmployeeService;
 import lombok.Data;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Objects;
 
 /**
  * @author Roch Blondiaux
@@ -36,9 +40,55 @@ public class EmployeeController {
      * @param id employee id.
      * @return An employee object fulfilled, or otherwise it throws an exception.
      */
-    @GetMapping("/employee/{id}")
+    @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable("id") int id) {
         return service.getById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(String.format("Cannot find any employee with id '%d'!", id)));
+    }
+
+    /**
+     * Create new employee.
+     *
+     * @param employee employee object to save.
+     * @return HTTP response status code with the created employee url.
+     */
+    @PostMapping("/employees")
+    public ResponseEntity<Void> createEmployee(@NonNull @RequestBody Employee employee) {
+        Employee newEmployee = service.save(employee);
+        if (Objects.isNull(newEmployee))
+            return ResponseEntity.noContent().build();
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newEmployee.getId())
+                .toUri();
+        return ResponseEntity.created(location)
+                .build();
+    }
+
+    /**
+     * Delete an existing employee.
+     *
+     * @param employee Employee object to delete.
+     * @return HTTP response status code.
+     */
+    @DeleteMapping("/employees")
+    public ResponseEntity<Void> deleteEmployee(@NonNull @RequestBody Employee employee) {
+        if (service.getById(employee.getId()).isEmpty())
+            return ResponseEntity.notFound().build();
+        service.delete(employee.getId());
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * Update an existing employee.
+     *
+     * @param employee to update.
+     * @return updated employee object.
+     */
+    @PatchMapping("/employees")
+    public Employee updateEmployee(@NonNull @RequestBody Employee employee) {
+        if (service.getById(employee.getId()).isEmpty())
+            throw new EmployeeNotFoundException(String.format("Cannot find any employee with id '%d'!", employee.getId()));
+        return service.save(employee);
     }
 }
--- src/test/java/com/roch/employees/controller/EmployeeControllerTest.java
+++ src/test/java/com/roch/employees/controller/EmployeeControllerTest.java
@@ -1,30 +1,147 @@
 package com.roch.employees.controller;
 
-import com.roch.employees.service.EmployeeService;
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.roch.employees.model.Employee;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Date;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author Roch Blondiaux
  * www.roch-blondiaux.com
  */
-@WebMvcTest(controllers = EmployeeController.class)
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@AutoConfigureMockMvc
 public class EmployeeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private EmployeeService service;
+    private static Employee validEmployee;
+    private static Employee invalidEmployee;
+    private static ObjectWriter mapper;
+
+    @BeforeAll
+    static void beforeAll() {
+        validEmployee = new Employee();
+        validEmployee.setFirstName("Test");
+        validEmployee.setLastName("Spring");
+        validEmployee.setMail("test@roch-blondiaux.com");
+        validEmployee.setHireDate(new Date());
+        validEmployee.setPassword("My Super Password");
+
+        invalidEmployee = new Employee();
+
+        mapper = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer()
+                .withDefaultPrettyPrinter();
+    }
+
+    @BeforeEach
+    void setUp() {
+        assert validEmployee != null;
+    }
 
     @Test
+    @Order(1)
+    public void testCreateEmployee() throws Exception {
+        mockMvc.perform(post("/employees")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(mapper.writeValueAsString(validEmployee))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isCreated());
+        validEmployee.setId(1);
+    }
+
+    @Test
+    @Order(2)
+    public void testGetSpecificEmployee() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/employees/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("firstName").value("Test"))
+                .andExpect(jsonPath("lastName").value("Spring"))
+                .andExpect(jsonPath("mail").value("test@roch-blondiaux.com"));
+    }
+
+    @Test
+    @Order(3)
+    public void testUpdateEmployee() throws Exception {
+        validEmployee.setFirstName("Hulk");
+        mockMvc.perform(patch("/employees")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(mapper.writeValueAsString(validEmployee))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("firstName").value("Hulk"));
+    }
+
+    @Test
+    @Order(4)
     public void testGetEmployees() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/employees"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$[0].firstName").value("Hulk"))
+                .andExpect(jsonPath("$[0].lastName").value("Spring"))
+                .andExpect(jsonPath("$[0].mail").value("test@roch-blondiaux.com"));
+    }
+
+    @Test
+    @Order(5)
+    public void testDeleteEmployee() throws Exception {
+        mockMvc.perform(delete("/employees")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(mapper.writeValueAsString(validEmployee))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/employees/1"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @Order(6)
+    public void testDeleteInvalidEmployee() throws Exception {
+        mockMvc.perform(delete("/employees")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(mapper.writeValueAsString(invalidEmployee))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Order(7)
+    public void testGetInvalidEmployee() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/employees/134"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @Order(8)
+    public void testUpdateInvalidEmployee() throws Exception {
+        mockMvc.perform(patch("/employees")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(mapper.writeValueAsString(invalidEmployee))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isNotFound());
     }
 }
